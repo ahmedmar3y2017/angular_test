44aebbe20708b0a7164839a925980e2413c1ca0d
ahmed mar3y <ahmedmar3y108@gmail.com>
2018-02-26 13:26:01 +0200

    i

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -4,15 +4,18 @@ import { AppComponent }  from './app.component';
 import { HttpModule } from '@angular/http';
 import { AppProductComponent } from './app-product/app-product.component';
 import { AppInventoryComponent } from './app-inventory/app-inventory.component';
+import { NotFoundComponent } from "./not-found/not-found.component";
 import { RouterModule, Routes } from '@angular/router';
 
 const appRoutes: Routes = [
+  
   { path: 'Product', component: AppProductComponent },
   { path: 'Inventory', component: AppInventoryComponent },
+  { path: '**', component: NotFoundComponent }
 ];
 @NgModule ({
   imports:      [ BrowserModule,HttpModule ,RouterModule.forRoot(appRoutes)],
-  declarations: [ AppComponent ,AppProductComponent ,AppInventoryComponent  ],
+  declarations: [ AppComponent ,AppProductComponent ,AppInventoryComponent ,NotFoundComponent  ],
   bootstrap:    [ AppComponent ]
 })
 export class AppModule { }
